c909e2399b2c75c4eba1bd534afb8a137193275d
john30 <ebusd@johnm.de>
2015-04-12 10:33:40 +0200

    fix for quoted CSV parts, added subnumbering of identical field names

--- etc/yawgd/tools/make_ebus_config.pl
+++ etc/yawgd/tools/make_ebus_config.pl
@@ -8,9 +8,22 @@ use warnings;
 use strict;
 use Getopt::Std;
 
-getopts('o:i:h:', \my %opts) or die "Please specify paths for config files!\n -i select ebusd-configfile (input) \n -o select plugin output-file (output) \n\nThe input file can be created by calling ebusd with the --dumpconfig argument, e.g.\n'ebusd --dumpconfig --logareas='\n";
-if ($opts{h}) {print "Please specify paths for config files!\n -i select ebusd-configfile (input) \n -o select plugin output-file (output) \n\nThe input file can be created by calling ebusd with the --dumpconfig argument, e.g.\n'ebusd --dumpconfig --logareas='\n"; exit;}
-if (!$opts{i} or !$opts{o}) {die "Please specify paths for config files!\n -i select ebusd-configfile (input) \n -o select plugin output-file (output) \n";}
+my $help =
+"Please specify paths for config files!\n".
+" -i select ebusd-configfile (input)\n".
+" -o select plugin output-file (output)\n".
+"\n".
+"The input file can be created by calling ebusd with the --dumpconfig argument, e.g.\n".
+"  'ebusd --dumpconfig --logareas='\n".
+"\n".
+"You can also query a running ebusd for all message configurations like this:\n".
+"  'ebusctl find -f'\n".
+"or without ebusctl:\n".
+"  'echo find -f|nc localhost 8888'\n";
+
+getopts('o:i:h:', \my %opts) or die $help;
+if ($opts{h}) {print $help; exit;}
+($opts{i} and $opts{o}) or die $help;
 my $config = $opts{i};
 print "Input: $config\n";
 my $final_cfg = $opts{o};
@@ -20,42 +33,66 @@ print "Output $final_cfg\n";
 my @cmds;
 open (GETCFG,"<$config") || die $!;
 while (<GETCFG>){
-  if ($_ =~ /\#/) {
+  if ($_ =~ /^\#/) {
     #if ($debug){plugin_log($plugname,"skipped line");}
   } else {
     #if ($debug){plugin_log($plugname,"line $_");}
+    chomp $_;
+    $_ =~ s/;/#/g;
+    $_ =~ s/^([^"]*)(,"[^",]*")?,"([^"]*),([^"]*)"/$1$2,"$3#$4"/g;
+    $_ =~ s/^([^"]*),"([^"]*)"/$1,$2/g;
     my @array = split (/,/,$_);
     my $type = $array[0];
     my $class = $array[1];
-    my $name = $array[2];
-    my $comment = $array[3];
-    my $elements = (@array - 8) / 6;
-    my $prefix = $class." ".$name;
-    my $cmd;
-    my $comm;
-    my $pos = 8;
-    my $cnt = 0;
-    for (my $i=0; $i < $elements; $i++, $pos+=6) {
-      if (substr($array[$pos+2], 0, 3) ne "IGN") {
-        $cnt++;
+    if (length $class > 0) {
+      my $name = $array[2];
+      my $comment = $array[3];
+      $comment =~ s/#/,/g;
+      my $elements = (@array - 8 + 5) / 6;
+      my $prefix = $class." ".$name;
+      my $cmd;
+      my $comm;
+      my $pos = 8;
+      my $cnt = 0;
+      my %cnts;
+      my %ccnts;
+      for (my $i=0; $i < $elements; $i++, $pos+=6) {
+        if (substr($array[$pos+2], 0, 3) ne "IGN") {
+          if ($cnt==0 or $array[$pos]) {
+            $cnt++;
+            if ($cnts{$array[$pos]}) {
+              $cnts{$array[$pos]}++;
+            } else {
+              $cnts{$array[$pos]} = 1;
+            }
+            $ccnts{$array[$pos]} = 0;
+          }
+        }
       }
-    }
-    $pos = 8;
-    for (my $i=0; $i < $elements; $i++, $pos+=6) {
-      if (substr($array[$pos+2], 0, 3) ne "IGN") {
-        if ($cnt>1 && $array[$pos]) {
-          $cmd = $prefix." ".$array[$pos];
-        } else {
-          $cmd = $prefix;
+      $pos = 8;
+      for (my $i=0, my $j=0; $i < $elements and $j < $cnt; $i++, $pos+=6) {
+        if (($cnt==1 or $array[$pos]) and (substr($array[$pos+2], 0, 3) ne "IGN")) {
+          if ($cnt>1 and $array[$pos]) {
+            $cmd = $prefix." ".$array[$pos];
+            if ($cnts{$array[$pos]} > 1) {
+              $cmd .= ".".$ccnts{$array[$pos]}++;
+            }
+          } else {
+            $cmd = $prefix;
+          }
+          if ($array[$pos+5]) {
+            $comm = $array[$pos+5];
+            $comm =~ s/#/,/g;
+            if (length $comment) {
+              $comm = $comment.". ".$comm;
+            }
+          } else {
+            $comm = $comment;
+          }
+          #print $type." ".$cmd." ".$comment."\n";
+          push @cmds,{type => $type, cmd => $cmd, comment => $comm};
+          $j++;
         }
-        if ($array[$pos+5]) {
-          $comm = $comment." ".$array[$pos+5];
-        } else {
-          $comm = $comment;
-        }
-        chomp $comm;
-        #print $type." ".$cmd." ".$comment."\n";
-        push @cmds,{type => $type, cmd => $cmd, comment => $comm};
       }
     }
   }
